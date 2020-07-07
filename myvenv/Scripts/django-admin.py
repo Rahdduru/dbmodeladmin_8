55a3ccc066eeca6e3458f60fc195614cb4d67be5
윤다혜 <smallgilr@gmail.com>
2020-07-07 20:06:33 +0900

    20.07.07

---
+++ myvenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\pochi_000\desktop\멋쟁이 사자처럼\강의\dbmodeladmin\dbmodeladmin\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
